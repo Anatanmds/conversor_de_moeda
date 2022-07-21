a6eab47ff54081c7838ec217bd5df623c61a76dc
Anatan Maia <anatanmaia3302@gmail.com>
2022-07-21 00:05:35 -0300

    API de cotações de Moedas
    Adição da Awesomeapi para consulta de moedas. Cotação do dólar atualizada a cada 30 segundos.

--- script.js
+++ script.js
@@ -1,24 +1,53 @@
 //Pegando dados de entrada
 var currencyType = document.querySelector("#currency")
-var currencyValue = document.querySelector("#value")
+var currency = document.querySelector("#value")
 var buttonConvert = document.querySelector("#btn_convert")
 var output = document.querySelector("span")
 
 buttonConvert.addEventListener("click", ()=> {
+  const resquest = new XMLHttpRequest()
 
-  if(currencyType.value == "brazilian") {
+  resquest.onreadystatechange = function () {
+    if(this.readyState == 4 && this.status == 200) {
+
+      let data = JSON.parse(this.response)
+      console.log(data.USDBRL.high)
+      let dollar = Number(data.USDBRL.high)
+
+      if(currencyType.value == "brazilian") {
+        let convert = currency.value/dollar
+    
+        output.innerText = `${convert.toLocaleString('pt-BR',{style: 'currency', currency:'USD'})}`
+      } 
+      else {
+        let convert = currency.value*dollar
+    
+        output.innerText = `${convert.toLocaleString('pt-BR',{style: 'currency', currency: 'BRL'})}`
+      }
+      
+    } 
+    else {
+      console.log("Erro na requisição vc é muito ruim!")
+    }
+  }
+
+  resquest.open("GET", `http://economia.awesomeapi.com.br/json/last/USD`)
+  resquest.send()
+
+
+
+
+  /*if(currencyType.value == "brazilian") {
     let convert = currencyValue.value/5.00
 
-    output.innerHTML = "&#x1F4B2"
-    output.innerText += `${convert} Dolares`
+    output.innerText = `${convert.toLocaleString('pt-BR',{style: 'currency', currency:'USD'})}`
   }
   else {
     let convert = currencyValue.value*5.00
 
-    output.innerHTML = "&#x1F4B2"
-    output.innerText += `${convert} Reais`
+    output.innerText = `${convert.toLocaleString('pt-BR',{style: 'currency', currency: 'BRL'})}`
   }
 
   console.log(currencyType.value)
-  console.log(currencyValue.value)
+  console.log(currencyValue.value)*/
 })
